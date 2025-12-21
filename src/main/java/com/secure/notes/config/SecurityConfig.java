012ed6d240e0c83a0c6fdfcd7035081b4a775e22
nyizeya <nyizeya103@gmail.com>
2025-12-21 19:00:13 +0630

    security: remove csrf validation

--- src/main/java/com/secure/notes/config/SecurityConfig.java
+++ src/main/java/com/secure/notes/config/SecurityConfig.java
@@ -15,11 +15,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -49,11 +49,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> {
-            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-            // Urls that do not need CSRF protection in POST requests
-            csrf.ignoringRequestMatchers(PUBLIC_URL);
-        });
+        http.csrf(AbstractHttpConfigurer::disable);
 
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
